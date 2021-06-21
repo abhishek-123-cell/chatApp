fb1588d384f9a121f2c1afd594440783fe0fb76c
abhishekupadhyay483@gmail.com <abhishekupadhyay483@gmail.com>
2021-06-21 13:52:51 +0530

    add

--- model/chat.js
+++ model/chat.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
-
-var messageSchema = new mongoose.Schema({
+const mongoose = require("mongoose"),
+  { Schema } = require("mongoose");
+var messageSchema = new Schema({
   to: { type: String, required: true },
   from: { type: String, required: true },
   message: { type: String, required: true },
